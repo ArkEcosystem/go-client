f0d9ce96eb65e0706ad8e3b98fdbd7a266957a32
Shahin Safaraliyev <shahin@ardenthq.com>
2026-07-30 20:26:22 +0400

    refactor: adjust transaction request types

--- client/blocks_requests.go
+++ client/blocks_requests.go
@@ -3,11 +3,10 @@ package client
 type BlocksQuery struct {
 	Pagination
 	OrderBy       string `url:"orderBy,omitempty"`
-	Id            string `url:"id,omitempty"`
+	Hash          string `url:"hash,omitempty"`
 	Number        int64  `url:"number,omitempty"`
-	Height        int64  `url:"height,omitempty"`
-	HeightFrom    int64  `url:"height.from,omitempty"`
-	HeightTo      int64  `url:"height.to,omitempty"`
+	NumberFrom    int64  `url:"number.from,omitempty"`
+	NumberTo      int64  `url:"number.to,omitempty"`
 	Timestamp     int64  `url:"timestamp,omitempty"`
 	TimestampFrom int64  `url:"timestamp.from,omitempty"`
 	TimestampTo   int64  `url:"timestamp.to,omitempty"`
